77b34ae51fcab14b4fb226f47cc059ccf0379d44
Hagen-Kwik <92912847+Hagen-Kwik@users.noreply.github.com>
2023-04-13 23:40:09 +0700

    section 4

--- AFL-3/AFL-3/Views/LandmarkList.swift
+++ AFL-3/AFL-3/Views/LandmarkList.swift
@@ -8,13 +8,27 @@
 import SwiftUI
 
 struct LandmarkList: View {
+    @State private var showFavoritesOnly = false
+
+    var filteredLandmarks: [Landmark] {
+        landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+    
     var body: some View {
         NavigationView {
-            List(landmarks) { landmark in
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites only")
+                }
+
+            ForEach(filteredLandmarks) { landmark in
                 NavigationLink {
                     LandmarkDetail(landmark: landmark)
-                } label: {
-                    LandmarkRow(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
                 }
             }
             .navigationTitle("Landmarks")
@@ -24,9 +38,10 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max", "iPad Pro"], id: \.self) { deviceName in LandmarkList()
-                .previewDevice(PreviewDevice(rawValue: deviceName))
-                .previewDisplayName(deviceName)
-        }
+//        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max", "iPad Pro"], id: \.self) { deviceName in LandmarkList()
+//                .previewDevice(PreviewDevice(rawValue: deviceName))
+//                .previewDisplayName(deviceName)
+//        }
+        LandmarkList()
     }
 }
