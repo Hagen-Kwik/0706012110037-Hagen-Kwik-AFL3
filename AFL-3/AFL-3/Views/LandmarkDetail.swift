98ea828fe05ef9c521709a9cce5a41b290700e25
Hagen-Kwik <92912847+Hagen-Kwik@users.noreply.github.com>
2023-04-13 23:17:47 +0700

    section 6

---
+++ AFL-3/AFL-3/Views/LandmarkDetail.swift
@@ -0,0 +1,54 @@
+//
+//  LandmarkDetail.swift
+//  AFL-3
+//
+//  Created by MacBook Pro on 13/04/23.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        VStack {
+            MapView()
+                .ignoresSafeArea(edges: .top)
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y:-130)
+                .frame(width: 50)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+
+                    Text("F1 Grand Prix")
+                        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .foregroundColor(.red)
+                    
+                HStack {
+                    Text("Monza Grand Prix")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("Italy")
+                        .font(.subheadline)
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                
+                Divider()
+                
+                Text("About Monza Circuit")
+                    .font(.title2)
+                Text("Monza was built in 1987 in Italy.")
+            }
+            .padding()
+            Spacer()
+        }
+    }
+}
+
+struct LandmarkDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkDetail()
+    }
+}
