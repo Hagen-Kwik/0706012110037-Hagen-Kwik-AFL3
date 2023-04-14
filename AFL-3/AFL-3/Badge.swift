1dad3d97170f88b7691a228354f8490e9e1f9977
Hagen-Kwik <92912847+Hagen-Kwik@users.noreply.github.com>
2023-04-14 08:57:41 +0700

    shape and path done

---
+++ AFL-3/AFL-3/Badge.swift
@@ -0,0 +1,20 @@
+//
+//  Badge.swift
+//  AFL-3
+//
+//  Created by MacBook Pro on 14/04/23.
+//
+
+import SwiftUI
+
+struct Badge: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Badge_Previews: PreviewProvider {
+    static var previews: some View {
+        Badge()
+    }
+}
