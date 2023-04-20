e84bfe8740588964753513af7a92bc5f982516a9
Hagen-Kwik <92912847+Hagen-Kwik@users.noreply.github.com>
2023-04-20 15:08:19 +0700

    section 4 composing complex interface

---
+++ AFL-3/AFL-3/Views/Categories/CategoryItem.swift
@@ -0,0 +1,30 @@
+//
+//  CategoryItem.swift
+//  AFL-3
+//
+//  Created by MacBook Pro on 20/04/23.
+//
+
+import SwiftUI
+
+struct CategoryItem: View {
+    var landmark: Landmark
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            landmark.image
+                .resizable()
+                .frame(width: 155, height: 155)
+                .cornerRadius(5)
+            Text(landmark.name)
+                .font(.caption)
+        }
+        .padding(.leading, 15)
+    }
+}
+
+struct CategoryItem_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryItem(landmark: ModelData().landmarks[0])
+    }
+}
